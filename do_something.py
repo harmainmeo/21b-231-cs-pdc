ce291292aae78b8085330fa6fa83cb2cc8c567e0
Hafiz Mohammad Harmain MEO <103923434+harmainmeo@users.noreply.github.com>
2025-11-08 16:34:49 +0500

    Delete do_something.py

--- do_something.py
+++
@@ -1,9 +0,0 @@
-
-import random
-
-def do_something(count):
-    """Generate a list of random numbers and return their sum."""
-    total = 0
-    for _ in range(count):
-        total += random.random()
-    return total
